e4c17b89697339d00a4f114070c3ceb27e8be1af
crus <romanghiet05@gmail.com>
2021-07-13 23:22:09 -0700

    project initialization

---
+++ CalculatorInSwift/CalculatorInSwift/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  CalculatorInSwift
+//
+//  Created by crus on 6/20/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var calText: UILabel!
+    @IBOutlet weak var outText: UILabel!
+    
+    var firstInput:Float?
+    var secondInput:Float?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+}
