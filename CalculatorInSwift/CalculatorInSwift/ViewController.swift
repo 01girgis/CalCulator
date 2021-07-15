780507b4760c64f22d92a32005a9613ccd5e8fc0
crus <romanghiet05@gmail.com>
2021-07-14 23:40:48 -0700

    rebase code logic with Decimal operations

--- CalculatorInSwift/CalculatorInSwift/ViewController.swift
+++ CalculatorInSwift/CalculatorInSwift/ViewController.swift
@@ -12,88 +12,84 @@ class ViewController: UIViewController {
     @IBOutlet weak var outText: UILabel!
     
     //Operations Variables
-        var firstInput:Double = 0
-        var secondInput:Double = 0
-        var mathCheckState:Bool = false
-        var mathOp: Int = 0
+    var firstInput:Double = 0
+    var secondInput:Double = 0
+    var mathCheckState:Bool = false
+    var mathOp: Int = 0
+    
+    //Mathematical Digits Action
+    @IBAction func inputNumbers(_ sender: UIButton) {
+        let getTag = sender.tag
         
-        //Mathematical Digits Action
-        @IBAction func inputNumbers(_ sender: UIButton) {
-            let getTag = sender.tag
-            
-            //Mathematical Operation
-            if mathCheckState == true {
-                outText.text = String(getTag)
-                mathCheckState = false
-            }
-            else{
-                        if getTag != 19{
-                            outText.text = outText.text! + String(getTag)
-                        }
-                        
-                        if getTag == 19 && outText.text?.last != "." && outText.text != ""{
-                            outText.text = outText.text! + "."
-                            calText.text?.append(".")
-                        }
-                    }
-                    if getTag < 10 && outText.text!.count > 2{
-                        calText.text?.append(String(getTag))
-                        outText.text = calText.text
-                        
-                    }
-
-                
-            //Second Input add
-            secondInput = Double(outText.text!)!
+        //Mathematical Operation
+        if mathCheckState == true {
+            outText.text = String(getTag)
+            mathCheckState = false
         }
         
-        //Mathematical Symbols Action
-        @IBAction func outputSymbols(_ sender: Any) {
-            let getTag = (sender as! UIButton ).tag
-            
-            //First Input add
-            if outText.text != "" && getTag != 10 && getTag != 18 {
-                firstInput =  Double(outText.text!)!
+        else{
+            if getTag != 19{
+                outText.text = outText.text! + String(getTag)
             }
-            
-            //clean process
-            if getTag == 10 {
-                outText.text = ""
-                calText.text = ""
-            }
-            
-            //Mathematical Symbols Settings
-            else if getTag == 14 {
-                outText.text = "+"
-                calText.text?.append("+")
-                mathOp = getTag
-                mathCheckState = true
-            }
-            else if getTag == 15 {
-                outText.text = "-"
-                calText.text?.append("-")
-                mathOp = getTag
-                mathCheckState = true
-            }
-            else if getTag == 16 {
-                outText.text = "*"
-                calText.text?.append("*")
-                mathOp = getTag
-                mathCheckState = true
-            }
-            else if getTag == 17 {
-                outText.text = "/"
-                calText.text?.append("/")
-                mathOp = getTag
-                mathCheckState = true
-            }
-            
-            //Equal Button
-            else if getTag == 18 {
-          
-
+            //Decimal calculation logic
+            if getTag == 19 && outText.text?.last != "." && outText.text != ""{
+                outText.text = outText.text! + "."
+                calText.text?.append(".")
             }
         }
+        
+        
+        //Second Input add
+        secondInput = Double(outText.text!)!
+    }
+    
+    //Mathematical Symbols Action
+    @IBAction func outputSymbols(_ sender: Any) {
+        let getTag = (sender as! UIButton ).tag
+        
+        //First Input add
+        if outText.text != "" && getTag != 10 && getTag != 18 {
+            firstInput =  Double(outText.text!)!
+        }
+        
+        //clean process
+        if getTag == 10 {
+            outText.text = ""
+            calText.text = ""
+        }
+        
+        //Mathematical Symbols Settings
+        else if getTag == 14 {
+            outText.text = "+"
+            calText.text?.append("+")
+            mathOp = getTag
+            mathCheckState = true
+        }
+        else if getTag == 15 {
+            outText.text = "-"
+            calText.text?.append("-")
+            mathOp = getTag
+            mathCheckState = true
+        }
+        else if getTag == 16 {
+            outText.text = "*"
+            calText.text?.append("*")
+            mathOp = getTag
+            mathCheckState = true
+        }
+        else if getTag == 17 {
+            outText.text = "/"
+            calText.text?.append("/")
+            mathOp = getTag
+            mathCheckState = true
+        }
+        
+        //Equal Button
+        else if getTag == 18 {
+            
+            
+        }
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
